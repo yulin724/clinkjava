0d24fe7eb01fc3c5819180b0bc6ed60f83f5d891
yulin724 <yulin724@yulin724-TW9-SW9.(none)>
2012-12-14 07:52:06 +0800

    first change

---
+++ tools/control-point/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -x
+
+java -classpath /home/yulin724/repo/CyberLink4Java/upnp-stack/target/upnp-stack-2.1.0-SNAPSHOT.jar:.:/home/yulin724/.m2/repository/net/sf/kxml/kxml2/2.1.8/kxml2-2.1.8.jar CtrlPoint
